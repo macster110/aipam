d7d33dfdae13b988a015287cf7afe569b556b4c8
jamie <macster110@gmail.com>
2019-01-17 10:30:07 +0100

    New build

--- AiPAM_builds/runAiPam.bat
+++ AiPAM_builds/runAiPam.bat
@@ -1,5 +1,5 @@
 @echo off
-set path= C:\Program Files\Java\jre1.8.0_191\bin\%path%
+set path= C:\Program Files\Java\jre1.8.0_131\bin\%path%
 java -version
 java -Xmx4098m -Dprism.maxvram=2000m -jar aipam.jar
 pause
